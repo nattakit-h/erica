e32020a6dacc2a83135b876536624ec082fde885
Nattakit Hosapsin <nattakit@hosapsin.com>
2022-08-22 15:29:20 +0700

    Migrate from straight.el to elpaca.el

--- init.el
+++ init.el
@@ -47,20 +47,32 @@
 
 ;;; Packages ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq straight-base-dir erica-data-directory)
-(setq straight-check-for-modifications '(check-on-save))
-
-(defvar bootstrap-version)
-(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(declare-function elpaca-generate-autoloads "elpaca")
+(defvar elpaca-directory (expand-file-name "elpaca/" erica-data-directory))
+(when-let ((elpaca-repo (expand-file-name "repos/elpaca/" elpaca-directory))
+           (elpaca-build (expand-file-name "builds/elpaca/" elpaca-directory))
+           (elpaca-target (if (file-exists-p elpaca-build) elpaca-build elpaca-repo))
+           (elpaca-url  "https://www.github.com/progfolio/elpaca.git")
+           ((add-to-list 'load-path elpaca-target))
+           ((not (file-exists-p elpaca-repo)))
+           (buffer (get-buffer-create "*elpaca-bootstrap*")))
+  (condition-case-unless-debug err
+      (progn
+        (unless (zerop (call-process "git" nil buffer t "clone" elpaca-url elpaca-repo))
+          (error "%s" (list (with-current-buffer buffer (buffer-string)))))
+        (byte-recompile-directory elpaca-repo 0 'force)
+        (require 'elpaca)
+        (elpaca-generate-autoloads "elpaca" elpaca-repo)
+        (kill-buffer buffer))
+    ((error)
+     (delete-directory elpaca-directory 'recursive)
+     (with-current-buffer buffer
+       (goto-char (point-max))
+       (insert (format "\n%S" err))
+       (display-buffer buffer)))))
+(require 'elpaca-autoloads)
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca (elpaca :host github :repo "progfolio/elpaca"))
 
 ;;; Files ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -70,18 +82,16 @@
 
 (defvar no-littering-var-directory erica-data-directory)
 (defvar no-littering-etc-directory erica-config-directory)
-(straight-use-package 'no-littering)
-(require 'no-littering)
 
-(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
-(setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+(elpaca no-littering
+  (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+  (when (file-exists-p custom-file) (load-file custom-file))
 
-(when (file-exists-p custom-file)
-  (load-file custom-file))
+  (with-eval-after-load 'recentf
+    (add-to-list 'recentf-exclude no-littering-var-directory)
+    (add-to-list 'recentf-exclude no-littering-etc-directory)))
 
-(with-eval-after-load 'recentf
-  (add-to-list 'recentf-exclude no-littering-var-directory)
-  (add-to-list 'recentf-exclude no-littering-etc-directory))
 
 (add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
 
@@ -150,7 +160,7 @@
 (setq uniquify-after-kill-buffer-p t)
 (setq uniquify-ignore-buffers-re "^\\*")
 
-;; todos
+;; ;; todos
 
 (defvar hl-todo-highlight-punctuation ":")
 (defvar hl-todo-keyword-faces
@@ -161,52 +171,51 @@
     ("HACK" error bold)
     ("BUG" error bold)
     ("XXX" error bold)))
-(straight-use-package 'hl-todo)
-(global-hl-todo-mode 1)
+(elpaca hl-todo
+  (global-hl-todo-mode 1))
 
 ;; ligaures
 
-(straight-use-package 'ligature)
-(ligature-set-ligatures
- t
- '("<--" "<---" "<<-" "<-" "->" "->>" "-->" "--->"
-   "<->" "<-->" "<--->" "<---->" "<!--"
-   "<==" "<===" "<<=" "<=" "=>" "=>>" "==>" "===>" ">=" ">>="
-   "<=>" "<==>" "<===>" "<====>" "<!---"
-    "<~~" "<~" "~>" "~~>" "::" ":::" "<>" ":>"
-    ":=" ":-" ":+" "<|" "<|>" "|>" ))
-(global-ligature-mode 1)
+(elpaca ligature
+  (ligature-set-ligatures
+   t
+   '("<--" "<---" "<<-" "<-" "->" "->>" "-->" "--->"
+     "<->" "<-->" "<--->" "<---->" "<!--"
+     "<==" "<===" "<<=" "<=" "=>" "=>>" "==>" "===>" ">=" ">>="
+     "<=>" "<==>" "<===>" "<====>" "<!---"
+     "<~~" "<~" "~>" "~~>" "::" ":::" "<>" ":>"
+     ":=" ":-" ":+" "<|" "<|>" "|>" ))
+  (global-ligature-mode 1))
 
 ;;; Completion ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar savehist-additional-variables '(command-history))
 (savehist-mode 1)
 
-(straight-use-package 'orderless)
-(setq completion-styles '(orderless))
+(elpaca orderless
+  (setq completion-styles '(orderless)))
 
-(straight-use-package 'vertico)
-(vertico-mode 1)
-(setq minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+(elpaca vertico
+  (vertico-mode 1)
+  (setq minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
 
 (defvar corfu-auto t)
 (defvar corfu-quit-at-boundary t)
 (defvar corfu-quit-no-match t)
-(straight-use-package 'corfu)
+(elpaca corfu)
 
-(straight-use-package 'marginalia)
-(marginalia-mode)
+(elpaca marginalia
+  (marginalia-mode))
 
-(straight-use-package 'page-break-lines)
-(global-page-break-lines-mode 1)
+(elpaca page-break-lines
+  (global-page-break-lines-mode 1))
 
 ;;; Compilation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar compile-command (format "%s%s%s" "make -j" (+ 1 (num-processors)) " --no-print-directory -Cbuild"))
 (defvar fancy-compilation-override-colors nil)
-(straight-use-package
- '(fancy-compilation :type git :host nil :repo "https://codeberg.org/ideasman42/emacs-fancy-compilation.git"))
+(elpaca '(fancy-compilation :repo "https://codeberg.org/ideasman42/emacs-fancy-compilation.git"))
 (keymap-global-set "<f5>" #'recompile)
 (with-eval-after-load 'compilation
   (advice-add 'compile :after (lambda (&rest _) (call-interactively 'other-window)))
@@ -266,60 +275,63 @@
 (setq-default indent-tabs-mode nil)
 
 (defvar hungry-delete-chars-to-skip " \t")
-(straight-use-package 'hungry-delete)
-(global-hungry-delete-mode 1)
+(elpaca hungry-delete
+  (global-hungry-delete-mode 1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Major Modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(straight-use-package 'cmake-mode)
-(straight-use-package 'csharp-mode)
-(straight-use-package 'glsl-mode)
-(straight-use-package 'go-mode)
-(straight-use-package 'yaml-mode)
+(elpaca cmake-mode)
+(elpaca csharp-mode)
+(elpaca glsl-mode)
+(elpaca go-mode)
+(elpaca yaml-mode)
 
 (defvar font-latex-fontify-sectioning 'color)
-(straight-use-package 'auctex)
+(elpaca auctex)
 
-(straight-use-package 'scribble-mode)
-(straight-use-package 'racket-mode)
-(add-hook 'racket-before-run-hook #'racket-repl-clear)
+(elpaca scribble-mode)
+(elpaca racket-mode
+  (add-hook 'racket-before-run-hook #'racket-repl-clear))
 
 (defvar geiser-chez-binary
   (cl-some (lambda (name) (executable-find name))
            '("chezscheme" "chez" "chez-scheme")))
-(straight-use-package 'geiser)
-(straight-use-package 'geiser-chez)
-(add-to-list 'auto-mode-alist '("\\.sls\\'" . scheme-mode))
-(with-eval-after-load 'geiser
-  (put 'module 'scheme-indent-function 1)
-  (put 'and-let* 'scheme-indent-function 1)
-  (put 'parameterize 'scheme-indent-function 1)
-  (put 'handle-exceptions 'scheme-indent-function 1)
-  (put 'when 'scheme-indent-function 1)
-  (put 'unless 'scheme-indent-function 1)
-  (put 'match 'scheme-indent-function 1))
+
+(elpaca geiser
+  (add-to-list 'auto-mode-alist '("\\.sls\\'" . scheme-mode))
+  (with-eval-after-load 'geiser
+    (elpaca geiser-chez)
+
+    (put 'module 'scheme-indent-function 1)
+    (put 'and-let* 'scheme-indent-function 1)
+    (put 'parameterize 'scheme-indent-function 1)
+    (put 'handle-exceptions 'scheme-indent-function 1)
+    (put 'when 'scheme-indent-function 1)
+    (put 'unless 'scheme-indent-function 1)
+    (put 'match 'scheme-indent-function 1)))
 
 ;;; Eglot  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar eglot-autoshutdown t)
 (defvar eglot-send-changes-idle-time 0.25)
-(straight-use-package 'eglot)
-(keymap-global-set "<f8>" #'eglot)
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '((c-mode c++-mode) . ("clangd")))
-  (add-to-list 'eglot-ignored-server-capabilites :hoverProvider)
-  (add-hook 'before-save-hook
-	    (lambda ()
-	      (when (eglot-managed-p)
-		(call-interactively #'eglot-format-buffer)))))
+(elpaca eglot
+  (keymap-global-set "<f8>" #'eglot)
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs '((c-mode c++-mode) . ("clangd")))
+    (add-to-list 'eglot-ignored-server-capabilites :hoverProvider)
+    (add-hook 'before-save-hook
+              (lambda ()
+                (when (eglot-managed-p)
+		          (call-interactively #'eglot-format-buffer))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tools
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq vc-follow-symlinks t)
 (setq project-vc-merge-submodules nil)
 
 (setq dired-listing-switches "-laGh1v --group-directories-first --time-style=long-iso")
@@ -327,35 +339,35 @@
 
 (when (executable-find "git")
   (defvar magit-auto-revert-mode nil)
-  (straight-use-package 'magit)
-  (straight-use-package 'magit-todos)
-  (add-hook 'magit-status-mode-hook #'magit-todos-mode))
+  (elpaca magit)
+  (elpaca magit-todos
+    (add-hook 'magit-status-mode-hook #'magit-todos-mode)))
 
 (when (executable-find "rg")
-  (straight-use-package 'rg)
+  (elpaca rg)
   (keymap-global-set "C-x p g" #'rg-project)
   (with-eval-after-load 'rg
     (add-to-list 'rg-custom-type-aliases '("el" . "*.el"))
     (add-to-list 'rg-custom-type-aliases '("ss" . "*.ss *.scm *.sls *.sld"))))
 
-;;; Documents Viewers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;; Documents Viewers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(straight-use-package 'pdf-tools)
-(autoload 'pdf-view-mode "pdf-tools")
-(add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
-(add-hook 'straight-use-package-post-build-functions
-          (lambda (name)
-            (when (string= "pdf-tools" name)
-              (pdf-tools-install t t))))
+(elpaca pdf-tools
+  (autoload 'pdf-view-mode "pdf-tools")
+  (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
+  (add-hook 'straight-use-package-post-build-functions
+            (lambda (name)
+              (when (string= "pdf-tools" name)
+		        (pdf-tools-install t t)))))
 
 
 (defvar visual-fill-column-center-text t)
-(straight-use-package 'visual-fill-column)
+(elpaca visual-fill-column)
 
 
 (defvar nov-text-width 120)
 (defvar nov-save-place-file (expand-file-name "nov-places" erica-data-directory))
-(straight-use-package 'nov)
+(elpaca nov)
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (defun erica-setup-nov-mode ()
   (setq-local fill-column 140)
@@ -485,95 +497,95 @@
  t)      ; SIMPLE
 
 (quail-define-rules
- ("\\ga" ?α)	; GREEK SMALL LETTER ALPHA
- ("\\gb" ?β)	; GREEK SMALL LETTER BETA
- ("\\gg" ?γ)	; GREEK SMALL LETTER GAMMA
- ("\\gd" ?δ)	; GREEK SMALL LETTER DELTA
- ("\\ge" ?ε)	; GREEK SMALL LETTER EPSILON
- ("\\gz" ?ζ)	; GREEK SMALL LETTER ZETA
- ("\\gy" ?η)	; GREEK SMALL LETTER ETA
- ("\\gh" ?θ)	; GREEK SMALL LETTER THETA
- ("\\gi" ?ι)	; GREEK SMALL LETTER IOTA
- ("\\gk" ?κ)	; GREEK SMALL LETTER KAPPA
- ("\\gl" ?λ)	; GREEK SMALL LETTER LAMDA
- ("\\gm" ?μ)	; GREEK SMALL LETTER MU
- ("\\gn" ?ν)	; GREEK SMALL LETTER NU
- ("\\gc" ?ξ)	; GREEK SMALL LETTER XI
- ("\\go" ?ο)	; GREEK SMALL LETTER OMNICRON
- ("\\gp" ?π)	; GREEK SMALL LETTER PI
- ("\\gr" ?ρ)	; GREEK SMALL LETTER RHO
- ("\\gs" ?σ)	; GREEK SMALL LETTER SIGMA
- ("\\gt" ?τ)	; GREEK SMALL LETTER TAU
- ("\\gu" ?υ)	; GREEK SMALL LETTER UPSILON
- ("\\gf" ?φ)	; GREEK SMALL LETTER PHI
- ("\\gx" ?χ)	; GREEK SMALL LETTER CHI
- ("\\gq" ?ψ)	; GREEK SMALL LETTER PSI
- ("\\gw" ?ω)	; GREEK SMALL LETTER OMEGA
+ ("\\ga" ?α)    ; GREEK SMALL LETTER ALPHA
+ ("\\gb" ?β)    ; GREEK SMALL LETTER BETA
+ ("\\gg" ?γ)    ; GREEK SMALL LETTER GAMMA
+ ("\\gd" ?δ)    ; GREEK SMALL LETTER DELTA
+ ("\\ge" ?ε)    ; GREEK SMALL LETTER EPSILON
+ ("\\gz" ?ζ)    ; GREEK SMALL LETTER ZETA
+ ("\\gy" ?η)    ; GREEK SMALL LETTER ETA
+ ("\\gh" ?θ)    ; GREEK SMALL LETTER THETA
+ ("\\gi" ?ι)    ; GREEK SMALL LETTER IOTA
+ ("\\gk" ?κ)    ; GREEK SMALL LETTER KAPPA
+ ("\\gl" ?λ)    ; GREEK SMALL LETTER LAMDA
+ ("\\gm" ?μ)    ; GREEK SMALL LETTER MU
+ ("\\gn" ?ν)    ; GREEK SMALL LETTER NU
+ ("\\gc" ?ξ)    ; GREEK SMALL LETTER XI
+ ("\\go" ?ο)    ; GREEK SMALL LETTER OMNICRON
+ ("\\gp" ?π)    ; GREEK SMALL LETTER PI
+ ("\\gr" ?ρ)    ; GREEK SMALL LETTER RHO
+ ("\\gs" ?σ)    ; GREEK SMALL LETTER SIGMA
+ ("\\gt" ?τ)    ; GREEK SMALL LETTER TAU
+ ("\\gu" ?υ)    ; GREEK SMALL LETTER UPSILON
+ ("\\gf" ?φ)    ; GREEK SMALL LETTER PHI
+ ("\\gx" ?χ)    ; GREEK SMALL LETTER CHI
+ ("\\gq" ?ψ)    ; GREEK SMALL LETTER PSI
+ ("\\gw" ?ω)    ; GREEK SMALL LETTER OMEGA
 
- ("\\gA" ?Α)	; GREEK CAPITAL LETTER ALPHA
- ("\\gB" ?Β)	; GREEK CAPITAL LETTER BETA
- ("\\gG" ?Γ)	; GREEK CAPITAL LETTER GAMMA
- ("\\gD" ?Δ)	; GREEK CAPITAL LETTER DELTA
- ("\\gE" ?Ε)	; GREEK CAPITAL LETTER EPSILON
- ("\\gZ" ?Ζ)	; GREEK CAPITAL LETTER ZETA
- ("\\gY" ?Η)	; GREEK CAPITAL LETTER ETA
- ("\\gH" ?Θ)	; GREEK CAPITAL LETTER THETA
- ("\\gI" ?Ι)	; GREEK CAPITAL LETTER IOTA
- ("\\gK" ?Κ)	; GREEK CAPITAL LETTER KAPPA
- ("\\gL" ?Λ)	; GREEK CAPITAL LETTER LAMDA
- ("\\gM" ?Μ)	; GREEK CAPITAL LETTER MU
- ("\\gN" ?Ν)	; GREEK CAPITAL LETTER NU
- ("\\gC" ?Ξ)	; GREEK CAPITAL LETTER XI
- ("\\gO" ?Ο)	; GREEK CAPITAL LETTER OMNICRON
- ("\\gP" ?Π)	; GREEK CAPITAL LETTER PI
- ("\\gR" ?Ρ)	; GREEK CAPITAL LETTER RHO
- ("\\gS" ?Σ)	; GREEK CAPITAL LETTER SIGMA
- ("\\gT" ?Τ)	; GREEK CAPITAL LETTER TAU
- ("\\gU" ?Υ)	; GREEK CAPITAL LETTER UPSILON
- ("\\gF" ?Φ)	; GREEK CAPITAL LETTER PHI
- ("\\gX" ?Χ)	; GREEK CAPITAL LETTER CHI
- ("\\gQ" ?Ψ)	; GREEK CAPITAL LETTER PSI
- ("\\gW" ?Ω)	; GREEK CAPITAL LETTER OMEGA
+ ("\\gA" ?Α)    ; GREEK CAPITAL LETTER ALPHA
+ ("\\gB" ?Β)    ; GREEK CAPITAL LETTER BETA
+ ("\\gG" ?Γ)    ; GREEK CAPITAL LETTER GAMMA
+ ("\\gD" ?Δ)    ; GREEK CAPITAL LETTER DELTA
+ ("\\gE" ?Ε)    ; GREEK CAPITAL LETTER EPSILON
+ ("\\gZ" ?Ζ)    ; GREEK CAPITAL LETTER ZETA
+ ("\\gY" ?Η)    ; GREEK CAPITAL LETTER ETA
+ ("\\gH" ?Θ)    ; GREEK CAPITAL LETTER THETA
+ ("\\gI" ?Ι)    ; GREEK CAPITAL LETTER IOTA
+ ("\\gK" ?Κ)    ; GREEK CAPITAL LETTER KAPPA
+ ("\\gL" ?Λ)    ; GREEK CAPITAL LETTER LAMDA
+ ("\\gM" ?Μ)    ; GREEK CAPITAL LETTER MU
+ ("\\gN" ?Ν)    ; GREEK CAPITAL LETTER NU
+ ("\\gC" ?Ξ)    ; GREEK CAPITAL LETTER XI
+ ("\\gO" ?Ο)    ; GREEK CAPITAL LETTER OMNICRON
+ ("\\gP" ?Π)    ; GREEK CAPITAL LETTER PI
+ ("\\gR" ?Ρ)    ; GREEK CAPITAL LETTER RHO
+ ("\\gS" ?Σ)    ; GREEK CAPITAL LETTER SIGMA
+ ("\\gT" ?Τ)    ; GREEK CAPITAL LETTER TAU
+ ("\\gU" ?Υ)    ; GREEK CAPITAL LETTER UPSILON
+ ("\\gF" ?Φ)    ; GREEK CAPITAL LETTER PHI
+ ("\\gX" ?Χ)    ; GREEK CAPITAL LETTER CHI
+ ("\\gQ" ?Ψ)    ; GREEK CAPITAL LETTER PSI
+ ("\\gW" ?Ω)    ; GREEK CAPITAL LETTER OMEGA
 
- ("\\^0" ?⁰)	; SUPERSCRIPT DIGIT ZERO
- ("\\^1" ?¹)	; SUPERSCRIPT DIGIT ONE
- ("\\^2" ?²)	; SUPERSCRIPT DIGIT TWO
- ("\\^3" ?³)	; SUPERSCRIPT DIGIT THREE
- ("\\^4" ?⁴)	; SUPERSCRIPT DIGIT FOUR
- ("\\^5" ?⁵)	; SUPERSCRIPT DIGIT FIVE
- ("\\^6" ?⁶)	; SUPERSCRIPT DIGIT SIX
- ("\\^7" ?⁷)	; SUPERSCRIPT DIGIT SEVEN
- ("\\^8" ?⁸)	; SUPERSCRIPT DIGIT EIGHT
- ("\\^9" ?⁹)	; SUPERSCRIPT DIGIT NINE
- ("\\^+" ?⁺)	; SUPERSCRIPT PLUS SIGN
- ("\\^-" ?⁻)	; SUPERSCRIPT MINUS
- ("\\^=" ?⁼)	; SUPERSCRIPT EQUALS SIGN
- ("\\^(" ?⁽)	; SUPERSCRIPT LEFT PARENTHESIS
- ("\\^)" ?⁾)	; SUPERSCRIPT RIGHT PARENTHESIS
- ("\\^i" ?ⁱ)	; SUPERSCRIPT LATIN SMALL LETTER I
- ("\\^n" ?ⁿ)	; SUPERSCRIPT LATIN SMALL LETTER N
- 
- ("\\_0" ?₀)	; SUBSCRIPT ZERO
- ("\\_1" ?₁)	; SUBSCRIPT ONE
- ("\\_2" ?₂)	; SUBSCRIPT TWO
- ("\\_3" ?₃)	; SUBSCRIPT THREE
- ("\\_4" ?₄)	; SUBSCRIPT FOUR
- ("\\_5" ?₅)	; SUBSCRIPT FIVE
- ("\\_6" ?₆)	; SUBSCRIPT SIX
- ("\\_7" ?₇)	; SUBSCRIPT SEVEN
- ("\\_8" ?₈)	; SUBSCRIPT EIGHT
- ("\\_9" ?₉)	; SUBSCRIPT NINE
- ("\\_+" ?₊)	; SUBSCRIPT PLUS SIGN
- ("\\_-" ?₋)	; SUBSCRIPT MINUS
- ("\\_=" ?₌)	; SUBSCRIPT EQUALS SIGN
- ("\\_(" ?₍)	; SUBSCRIPT LEFT PARENTHESIS
- ("\\_)" ?₎)	; SUBSCRIPT RIGHT PARENTHESIS
+ ("\\^0" ?⁰)    ; SUPERSCRIPT DIGIT ZERO
+ ("\\^1" ?¹)    ; SUPERSCRIPT DIGIT ONE
+ ("\\^2" ?²)    ; SUPERSCRIPT DIGIT TWO
+ ("\\^3" ?³)    ; SUPERSCRIPT DIGIT THREE
+ ("\\^4" ?⁴)    ; SUPERSCRIPT DIGIT FOUR
+ ("\\^5" ?⁵)    ; SUPERSCRIPT DIGIT FIVE
+ ("\\^6" ?⁶)    ; SUPERSCRIPT DIGIT SIX
+ ("\\^7" ?⁷)    ; SUPERSCRIPT DIGIT SEVEN
+ ("\\^8" ?⁸)    ; SUPERSCRIPT DIGIT EIGHT
+ ("\\^9" ?⁹)    ; SUPERSCRIPT DIGIT NINE
+ ("\\^+" ?⁺)    ; SUPERSCRIPT PLUS SIGN
+ ("\\^-" ?⁻)    ; SUPERSCRIPT MINUS
+ ("\\^=" ?⁼)    ; SUPERSCRIPT EQUALS SIGN
+ ("\\^(" ?⁽)    ; SUPERSCRIPT LEFT PARENTHESIS
+ ("\\^)" ?⁾)    ; SUPERSCRIPT RIGHT PARENTHESIS
+ ("\\^i" ?ⁱ)    ; SUPERSCRIPT LATIN SMALL LETTER I
+ ("\\^n" ?ⁿ)    ; SUPERSCRIPT LATIN SMALL LETTER N
 
- ("\\ma" ?∀)	; FOR ALL
- ("\\mu" ?∪)	; UNION
- ("\\mi" ?∩)	; INTERSECTION
+ ("\\_0" ?₀)    ; SUBSCRIPT ZERO
+ ("\\_1" ?₁)    ; SUBSCRIPT ONE
+ ("\\_2" ?₂)    ; SUBSCRIPT TWO
+ ("\\_3" ?₃)    ; SUBSCRIPT THREE
+ ("\\_4" ?₄)    ; SUBSCRIPT FOUR
+ ("\\_5" ?₅)    ; SUBSCRIPT FIVE
+ ("\\_6" ?₆)    ; SUBSCRIPT SIX
+ ("\\_7" ?₇)    ; SUBSCRIPT SEVEN
+ ("\\_8" ?₈)    ; SUBSCRIPT EIGHT
+ ("\\_9" ?₉)    ; SUBSCRIPT NINE
+ ("\\_+" ?₊)    ; SUBSCRIPT PLUS SIGN
+ ("\\_-" ?₋)    ; SUBSCRIPT MINUS
+ ("\\_=" ?₌)    ; SUBSCRIPT EQUALS SIGN
+ ("\\_(" ?₍)    ; SUBSCRIPT LEFT PARENTHESIS
+ ("\\_)" ?₎)    ; SUBSCRIPT RIGHT PARENTHESIS
 
- ("\\\\" ?\\))	; REVERSE SOLIDUS
+ ("\\ma" ?∀)    ; FOR ALL
+ ("\\mu" ?∪)    ; UNION
+ ("\\mi" ?∩)    ; INTERSECTION
+
+ ("\\\\" ?\\))  ; REVERSE SOLIDUS
 
 (defun erica-cycle-input-method ()
   (interactive)
