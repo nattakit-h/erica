32f58a6debea46ae4ed85f6780a0d4adf539ca66
Nattakit Hosapsin <nattakit@hosapsin.com>
2023-01-21 06:26:43 +0700

    Add kind-icons for corfu

--- init.el
+++ init.el
@@ -185,6 +185,15 @@
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
+(use-package kind-icon
+  :after (corfu)
+  :defines (corfu-margin-formatters)
+  :functions (kind-icon-margin-formatter)
+  :custom
+  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
 ;; page-break
 
 (use-package page-break-lines
